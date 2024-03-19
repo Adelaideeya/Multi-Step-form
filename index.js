d8a8460f83541361ea3d83139d5c7eaca58dff5d
Adelaideeya <appianingadelaide@gmail.com>
2024-03-19 11:24:51 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// JavaScript function to show/hide form sections
+function showFormSection(sectionNumber) {
+    // Hide all form sections
+    var sections = document.querySelectorAll('.form-section');
+    sections.forEach(function(section) {
+      section.classList.remove('active');
+    });
+
+    // Show the selected form section
+    var selectedSection = document.getElementById('section' + sectionNumber);
+    selectedSection.classList.add('active');
+  }
+    
+  
